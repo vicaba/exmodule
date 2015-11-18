5002016116c79bc919cb975e94f2d5f0f364140a
vicaba <vicaba@me.com>
2015-11-18 22:24:27 +0100

    Again...

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,14 +1,5 @@
 	class exmodule::mysql {
 
-		    # Ensure Time Zone and Region.
-		class { 'timezone':
-			timezone => 'Europe/Madrid',
-		}
-
-		#NTP
-		class { '::ntp':
-			server => [ '1.es.pool.ntp.org', '2.europe.pool.ntp.org', '3.europe.pool.ntp.org' ],
-	}
 
 		# MYSQL
 	  	class { '::mysql::server':}
@@ -22,14 +13,6 @@
 		      user     => 'vagrant',
 		      password => 'vagrant'
 		  	}
-
-		  	#FIREWALL
-		  	firewalld_rich_rule { 'Accept HTTP':
-		      ensure  => present,
-		      zone    => 'public',
-		      service => 'http',
-		      action  => 'accept',
-		    }
 
 
 	  	}
