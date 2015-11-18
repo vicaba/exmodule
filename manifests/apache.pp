99cdb86f80f5c8157408cf6586dd8969b17336ed
vicaba <vicaba@me.com>
2015-11-18 22:21:55 +0100

    Otra vez...

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -1,14 +1,5 @@
 	class exmodule::apache {
 
-	    # Ensure Time Zone and Region.
-		class { 'timezone':
-			timezone => 'Europe/Madrid',
-		}
-
-		#NTP
-		class { '::ntp':
-			server => [ '1.es.pool.ntp.org', '2.europe.pool.ntp.org', '3.europe.pool.ntp.org' ],
-	}
 		firewalld_rich_rule { 'Accept HTTP':
 	      ensure  => present,
 	      zone    => 'public',
